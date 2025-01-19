25ca46bf36c6273090c69628e3e35e0961301e7a
Bastian Sievers <bastian.sievers@gmail.com>
2025-01-19 21:09:13 +0100

    early impl incl temp testing

--- main.go
+++ main.go
@@ -2,19 +2,79 @@ package main
 
 import (
 	"log"
+	"math/rand/v2"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
+
+var (
+	Domains map[string]Domain
+)
+
+type Domain struct {
+	DomainPath string
+	Subdomains map[string]Subdomain
+}
+
+type Subdomain struct {
+	SubdomainPath string
+	Servers       []string
+}
+
+func splitHostPort(host string) (subdomain, domain string) {
+	hostPortSplit := strings.Split(host, ":")
+
+	// TODO: handle basic Auth
+	if len(hostPortSplit) > 2 {
+		log.Panic("Basic Auth in URL not supported")
+	}
+
+	hostWoPort := hostPortSplit[0]
+
+	domainSplit := strings.Split(hostWoPort, ".")
+	subdomain = domainSplit[0]
+	domain = strings.Join(domainSplit[1:], ".")
+
+	return subdomain, domain
+}
 
 type ProxyHandler struct{}
 
 func (h *ProxyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	log.Printf("Received request: %s %s %s", req.Method, req.URL, req.Header)
-	target, err := url.Parse("http://httpbin.org/") // Replace with the actual target URL
-	if err != nil {
-		log.Println(err)
-		http.Error(res, "Invalid target URL", http.StatusInternalServerError)
+	var (
+		target *url.URL
+		err    error
+	)
+
+	log.Printf("Received request: %s %s %s", req.Method, req.URL, req.Host)
+	subdomain, domain := splitHostPort(req.Host)
+
+	if d, e := Domains[domain]; e {
+		log.Println(d)
+		if s, e := d.Subdomains[subdomain]; e {
+			log.Println(s)
+			var server string
+			if len(s.Servers) == 0 {
+				server = s.Servers[0]
+			} else {
+				server = s.Servers[rand.IntN(len(s.Servers))]
+			}
+
+			target, err = url.Parse(server)
+			if err != nil {
+				log.Println(err)
+				http.Error(res, "Invalid target URL", http.StatusInternalServerError)
+				return
+			}
+			log.Println("Target: ", target)
+		} else {
+			log.Println("No domain found")
+			return
+		}
+	} else {
+		log.Println("No domain found")
 		return
 	}
 
@@ -37,6 +97,34 @@ func (h *ProxyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	Domains = map[string]Domain{
+		"localhost": Domain{
+			DomainPath: "localhost",
+			Subdomains: map[string]Subdomain{
+				"test": Subdomain{
+					SubdomainPath: "test",
+					Servers:       []string{"http://httpbin.org"},
+				},
+				"host1": Subdomain{
+					SubdomainPath: "host1",
+					Servers:       []string{"http://localhost:9001"},
+				},
+				"host2": Subdomain{
+					SubdomainPath: "host2",
+					Servers:       []string{"http://localhost:9002"},
+				},
+				"random": Subdomain{
+					SubdomainPath: "random",
+					Servers: []string{
+						"http://httpbin.org",
+						"http://cht.sh",
+						"http://google.com",
+					},
+				},
+			},
+		},
+	}
+
 	// Start the HTTP server
 	port := "8080" // Replace with your desired port
 	log.Printf("Proxy server listening on port %s", port)
